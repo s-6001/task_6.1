22ae3091ad090d3649d37326252ece20d5db85c5
Эдуард Шамсиев <s-6001@yandex.ru>
2021-12-02 18:47:29 +0500

    Добавьте файлы проекта.

---
+++ task_6.1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace task_6._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Введите предложение без знаков препинания: ");
+                string input = Console.ReadLine();
+                string[] arr = input.Split(' ');
+                int maxLength = 0;
+                int index = 0;
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    if (arr[i].Length > maxLength)
+                    {
+                        maxLength = arr[i].Length;
+                        index = i;
+                    }
+                }
+                Console.WriteLine("Самое длинное слово(слова): ");
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    if (arr[i].Length == arr[index].Length)
+                    {
+                        Console.WriteLine(arr[i]);
+                    }
+                }
+            }
+        }
+    }
+}

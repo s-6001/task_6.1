d32ebf754ca7c7fb76f3917fdb9bfef12240248e
Эдуард Шамсиев <s-6001@yandex.ru>
2021-12-15 10:46:28 +0500

    убран второй цикл

--- task_6.1/Program.cs
+++ task_6.1/Program.cs
@@ -9,26 +9,19 @@ namespace task_6._1
             while (true)
             {
                 Console.Write("Введите предложение без знаков препинания: ");
-                string input = Console.ReadLine();
-                string[] arr = input.Split(' ');
-                int maxLength = 0;
-                int index = 0;
+                string input = Console.ReadLine();  //считываем строку
+                string[] arr = input.Split(' ');    //разделяем строку на массив слов, разделитель пробел
+                int maxLength = 0;  //максимальная длина слова
+                string longest = "";    //самое длинное слово
                 for (int i = 0; i < arr.Length; i++)
                 {
-                    if (arr[i].Length > maxLength)
+                    if (arr[i].Length > maxLength)  //сравниваем длину слова каждого элемента массива и присваиваем значения
                     {
                         maxLength = arr[i].Length;
-                        index = i;
+                        longest = arr[i];
                     }
                 }
-                Console.WriteLine("Самое длинное слово(слова): ");
-                for (int i = 0; i < arr.Length; i++)
-                {
-                    if (arr[i].Length == arr[index].Length)
-                    {
-                        Console.WriteLine(arr[i]);
-                    }
-                }
+                Console.WriteLine("Самое длинное слово: \"{0}\". Число букв: {1}", longest, maxLength);
             }
         }
     }
